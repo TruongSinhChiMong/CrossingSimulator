1211eb0ed1ad1e9e0e53db96a936c40240c98d76
TruongSinhChiMong <vinhgotowork@gmail.com>
2025-11-18 22:35:21 +0700

    chore: ignore Plastic SCM workspace files

---
+++ Assets/Scripts/Runtime/Spawners/PlayerSpawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public PlayerController playerPrefab;
+
+    void Start()
+    {
+        if (Object.FindFirstObjectByType<PlayerController>() != null) return;
+        var player = Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        player.name = "Player";
+    }
+}
